8e56784523d7d4be06aa91c6d3df6bd6e7a2d383
kidoo.han <kidoo.han@navercorp.com>
2019-04-29 13:54:30 +0900

    swagger 관련 주석 추가

--- src/server/routes/api/event-tracking/index.ts
+++ src/server/routes/api/event-tracking/index.ts
@@ -6,34 +6,50 @@ import { Tags } from 'typescript-rest-swagger';
 import {
     logger,
     base64Helper,
-} from '../../../libs'
+} from '../../../libs';
 
 @Path('/et')
-export class EventTrackerController {
+export class EventTrackingController {
+    /**
+     * Ack impression Event tracking.
+     * @param encryptedInfo
+     */
     @Path('/s1/:encryptedInfo')
     @GET
-    @Tags('event-tracker')
+    @Tags('event-tracking')
     invokeAckImpression(@PathParam('encryptedInfo') encryptedInfo: string) {
         logger.info(`[ack impression] ${base64Helper.decode(encryptedInfo)}`);
     }
 
+    /**
+     * Active view impression Event tracking.
+     * @param encryptedInfo
+     */
     @Path('/s2/:encryptedInfo')
     @GET
-    @Tags('event-tracker')
+    @Tags('event-tracking')
     invokeActiveViewImpression(@PathParam('encryptedInfo') encryptedInfo: string) {
         logger.info(`[active view impression] ${base64Helper.decode(encryptedInfo)}`);
     }
 
+    /**
+     * Click Event tracking.
+     * @param encryptedInfo
+     */
     @Path('/s3/:encryptedInfo')
     @GET
-    @Tags('event-tracker')
+    @Tags('event-tracking')
     invokeClick(@PathParam('encryptedInfo') encryptedInfo: string) {
         logger.info(`[click] ${base64Helper.decode(encryptedInfo)}`);
     }
 
+    /**
+     * Error event tracking.
+     * @param encryptedInfo
+     */
     @Path('/s99/:encryptedInfo')
     @GET
-    @Tags('event-tracker')
+    @Tags('event-tracking')
     invokeError(@PathParam('encryptedInfo') encryptedInfo: string) {
         logger.info(`[error] ${base64Helper.decode(encryptedInfo)}`);
     }
--- src/server/routes/api/index.ts
+++ src/server/routes/api/index.ts
@@ -2,9 +2,9 @@
  * Created by kidoo.han on 27/04/2019
  */
 import { AdRequestController } from './ad-request';
-import { EventTrackerController } from './event-tracker';
+import { EventTrackingController } from './event-tracking';
 
 export default [
     AdRequestController,
-    EventTrackerController,
+    EventTrackingController,
 ];
